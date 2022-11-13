c5457d8d733307fac351ab454346d364ec963b5a
mozixun <106295352+mozixun-me@users.noreply.github.com>
2022-11-13 21:22:11 +0800

    Update rules.mk

--- keyboards/crkbd/keymaps/mozixun/rules.mk
+++ keyboards/crkbd/keymaps/mozixun/rules.mk
@@ -1,5 +1,5 @@
 CONVERT_TO=kb2040
-MOUSEKEY_ENABLE = no     # Mouse keys
+MOUSEKEY_ENABLE = yes     # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 VIA_ENABLE      = yes    # Enable VIA
 VIAL_ENABLE     = yes
@@ -8,6 +8,6 @@ OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 WPM_ENABLE 		= yes
 VIAL_INSECURE   = yes
-QMK_SETTINGS    = no
-COMBO_ENABLE    = no
+QMK_SETTINGS    = yes
+COMBO_ENABLE    = yes
 KEY_OVERRIDE_ENABLE = no
